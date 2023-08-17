773507ab4c2c15c58a037eb6d69aa4f3549d2cc8
Wing <eamonn.kearns@so-4pt.net>
2023-08-17 07:54:20 +0100

    Further work on user logged in status. Begun implementation of localstorage to handle the client side of session handling

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,11 @@ const MOAP_CONTAINER = {
 
 };
 
+/**
+ * This is sufficiently verbose a function as to require its own function.
+ * @param password
+ * @returns {Promise<void>}
+ */
 async function decryptLoggedInUser(password)
 {
     const $feedback = $('#login_decryption_feedback');
@@ -33,7 +38,9 @@ async function decryptLoggedInUser(password)
         decryptedUserFields.operationAuthorisationApprovalNumber = await decrypt(user.operationAuthorisationApprovalNumber, encryptionKey);
     }
     MOAP_CONTAINER.decryptedUserFields = decryptedUserFields;
+    MOAP_CONTAINER.password = password;
     $('#loginModal').modal('hide');
+    // store the user data, including the password, in the localstorage
     localStorage.setItem('user', btoa(JSON.stringify(MOAP_CONTAINER)));
 }
 
@@ -48,6 +55,7 @@ function updateLoggedInStatus(loggedIn)
         $('.loginPhaseTwo').show();
         $('form .loggedInOnly').removeClass('d-none');
         $('#account-tab').text('Account details');
+        $('#logged_in_user_email').text(MOAP_CONTAINER.user.email);
     }
     else
     {
@@ -65,22 +73,43 @@ function updateLoggedInStatus(loggedIn)
 
     $(function()
     {
-
+        /*
+         * Check if the user is logged in on the server side
+         */
         fetch('/users/checkLoginStatus',{
             method:'POST'
         }).then(async response=>{
             const json = await response.json();
             if(json.user){
-
+                // if so, unpack the user
+                const userString = localStorage.getItem('user');
+                if(userString)
+                {
+                    const json = JSON.parse(atob(userString));
+                    for(const [key, value] of Object.entries(json))
+                    {
+                        MOAP_CONTAINER[key] = value;
+                    }
+                    await decryptLoggedInUser(MOAP_CONTAINER.password);
+                }
+                else
+                {
+                    // there's nothing in localstorage. It could have been cleared.
+                    console.log('Local storage empty. Logging out fully.')
+                    fetch('/users/logout',{method:'POST'}).then(()=>{
+                        window.location.reload();
+                    });
+                }
             }
             else
             {
+                // just double check that a logged out user has nothing in the localstore
                 localStorage.clear();
             }
         });
 
-        const hash = window.location.hash;
         // function to handle the url stuff
+        const hash = window.location.hash;
         $(".nav-tabs").find("li button").each(function(key, value) {
             let $button = $(value);
             let target = $button.data('bsTarget');
@@ -93,10 +122,12 @@ function updateLoggedInStatus(loggedIn)
             });
         });
 
+        // handle the logout button
         $('#logoutNav').click(function(){
             fetch('/users/logout',{
                 method:'POST'
             }).then(response=>{
+                localStorage.clear();
                 return response.json();
             }).then(responseJSON=>{
                 if(responseJSON.success)
@@ -107,32 +138,13 @@ function updateLoggedInStatus(loggedIn)
             });
         });
 
-        $('#login_form_login_button').click(function(){
-            const $elements = this.form.elements;
-
-
-            const formData = new FormData();
-            formData.append('email',$elements['login_form_email'].value);
-            formData.append('emailKey',$elements['login_form_email_key'].value);
-            formData.append('authKey', $elements['login_form_auth_key'].value);
-
-            fetch(
-                '/users/login',
-                {
-                    method:'POST',
-                    body:formData
-                }
-            ).then((response)=>{
-                return response.json();
-            }).then(async responseJSON=>{
-                if(responseJSON.success)
-                {
-                    MOAP_CONTAINER.user = responseJSON.user;
-                    updateLoggedInStatus(true);
-                }
-            });
-        });
-
+        /*
+        Handle the login process.
+        Part One.
+        This is actually an involved process. The login mechanism involves sending a request
+        to the server to get it to generate and email the user a login key. That key and a key from an authenticator
+        are then used to actually login
+         */
         $('#login_button').click(function(){
             const $form = this.form;
             const $elements = $form.elements;
@@ -151,16 +163,53 @@ function updateLoggedInStatus(loggedIn)
                         body: formData
                     }
                 ).then((response)=>{
+                    // I need a handler for this as unhandled promises will cause problems in future versions of JS
                     response.json().then(
                         (json)=>{
                             console.log(json);
                         }
                     );
                 });
+                // just show the login modal for the keys immediately. No point in waiting.
                 $('#loginModal').modal('show');
             }
         });
+        /*
+        Handle the login process.
+        Part Two.
+        Check the two keys against the TOTPs on the server
+         */
+        $('#login_form_login_button').click(function(){
+            const $elements = this.form.elements;
+            const formData = new FormData();
+            formData.append('email',$elements['login_form_email'].value);
+            formData.append('emailKey',$elements['login_form_email_key'].value);
+            formData.append('authKey', $elements['login_form_auth_key'].value);
 
+            fetch(
+                '/users/login',
+                {
+                    method:'POST',
+                    body:formData
+                }
+            ).then((response)=>{
+                return response.json();
+            }).then(async responseJSON=>{
+                if(responseJSON.success)
+                {
+                    // store the user in the DOM
+                    MOAP_CONTAINER.user = responseJSON.user;
+                    // update some fields
+                    updateLoggedInStatus(true);
+                }
+            });
+        });
+
+        /*
+        Handle the login process.
+        Part Three.
+        Decrypt the returned data
+         */
         $('#decrypt_button').click(function(){
             const [algorithm, digest, iterationsString, salt, hash] = MOAP_CONTAINER.user.passwordHash.split(':');
             const iterations = parseInt(iterationsString);
@@ -172,12 +221,5 @@ function updateLoggedInStatus(loggedIn)
                 await decryptLoggedInUser(password);
             });
         });
-
-        const userString = localStorage.getItem('user');
-        if(userString)
-        {
-            const json = JSON.parse(atob(userString));
-            console.log(json);
-        }
     });
 })(window.jQuery);
